26fbfe8e3556d2e6fc968ac0212b8c07513bbc1d
runsf <mclausaudio@gmail.com>
2018-08-21 07:14:25 +0000

    added collapsable left side bar and updated top nav

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,17 @@
+/* global $ */ 
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    alert('hi')
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
+
+$('.toggle-btn').click(function(e){
+    if($('.nav-left-side').hasClass('show')) {
+        $('.nav-left-side').collapse('hide');
+    } else {
+        $('.nav-left-side').collapse('show');
+    }
+});
+
+
+
+
